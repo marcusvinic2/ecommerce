246b3730666eb2659e807117524c49e8f6973d64
marcus <marcusvinic.br.154@gmail.com>
2023-02-07 18:11:35 -0300

    configs ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Link from 'next/link';
 import React from 'react'
 import Image from 'next/image';
 
+type ProductProps = {
+  id: Number;
+  title: string;
+  image: string;
+}
+
 async function getData() {
-  const response = await fetch(`https://fakestoreapi.com/products`, { next: { revalidate: 500 } });
+  const response = await fetch(`https://fakestoreapi.com/products`, { next: { revalidate: 10 } });
   return response.json();
 }
 
@@ -16,12 +22,12 @@ export default async function Home() {
     <div>
       {
         response !== null ? (
-          response.map((response: any, index: number) =>
+          response.map((response: ProductProps, index: number) =>
             <div>
               <Link href={`produto/${response.id}`}>
                 <h4>{response.title}</h4>
                 <div>
-                  <Image src={response.image} width={300} height={300} alt="product" />
+                  {response.image === null ? (<>loading...</>) : (<Image src={response.image} width={300} height={300} alt="product" placeholder="blur" />)}
                 </div>
               </Link>
             </div>
